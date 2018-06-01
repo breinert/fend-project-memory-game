a48ed4a94d8a086eaeb9b5630cd32eff0fc6de1d
Brian Reinert <breinert971@gmail.com>
2018-06-01 11:09:18 -0400

    edit: add modal for end of game

--- js/app.js
+++ js/app.js
@@ -5,20 +5,26 @@ const deck = document.querySelector('.deck');
 const card = document.getElementsByClassName('card');
 const cards = [...card];
 let moves = 0;
+let score = 3;
 let openedCards = [];
+let matched = 0;
 let moveCounter = document.querySelector('.moves');
 let stars = document.querySelector('.stars').querySelectorAll('i');
+let myModal = document.getElementById('modal');
 let start;
 let elapsed = 0;
 const min = document.querySelector('.min');
 const sec = document.querySelector('.sec');
-
-window.onload = initGame();
+const endScore = document.querySelector('.score');
+const endMoves = document.querySelector('.move');
+const mins = document.querySelector('.mins');
+const secs = document.querySelector('.secs');
 
 function initGame() {
     //Reset variables
     seconds = 0;
     moves = 0;
+    score = 3;
 
     moveCounter.innerText = moves;
     stars.forEach(function(star) {
@@ -39,8 +45,10 @@ function initGame() {
     for (let shuffleCard of shuffleCards) {
         deck.appendChild(shuffleCard);
     }
-    sec.innerHTML=0;
-    min.innerHTML=0;
+    sec.innerHTML = '0';
+    min.innerHTML = '0';
+
+    myModal.style.display = 'none';
 }
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -57,17 +65,32 @@ function shuffle(array) {
     return array;
 }
 
+//Timer function
 let timer = setInterval(function() {
-
     let time = new Date().getTime() - start;
     elapsed = Math.floor(time / 100) / 10;
     sec.innerHTML = (parseInt(elapsed%60));
     min.innerHTML = (parseInt(elapsed/60));
 }, 100);
 
+//Game over, display modal
+function openModal() {
+    const endMin = document.querySelector('.min').innerHTML;
+    const endSec = document.querySelector('.sec').innerHTML;
+    endScore.innerHTML = score;
+    mins.innerHTML = endMin;
+    secs.innerHTML = endSec;
+    endMoves.innerHTML = moves;
+    myModal.style.display = 'block';
+    clearInterval(timer);
+
+}
+
 document.getElementsByClassName('restart')[0].addEventListener('click', initGame);
 const currentCard = document.getElementsByClassName('card');
 const currentCards = [...currentCard];
+
+window.onload = initGame();
 
 currentCards.forEach(function(card) {
     card.addEventListener('click', function(evt) {
@@ -83,6 +106,10 @@ currentCards.forEach(function(card) {
                     openedCards[0].classList.add('match');
                     openedCards[1].classList.add('match');
                     openedCards = [];
+                    matched += 1;
+                    if (matched == 8) {
+                        openModal();
+                    }
                 } else {
                     //If no match, flip cards
                     setTimeout(function() {
@@ -94,11 +121,13 @@ currentCards.forEach(function(card) {
                 }
                 moves += 1;
                 moveCounter.innerText = moves;
-                if (moves % 14 == 0) {
+                if (moves == 14) {
                     stars[0].style.fontSize = '0';
+                    score = 2;
                 }
-                if (moves % 22 == 0) {
+                if (moves == 22) {
                     stars[1].style.fontSize = '0';
+                    score = 1;
                 }
             }
         }
